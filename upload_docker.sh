5b87f9b884eea543bbfa064029f94b25282e6423
eslam-adel92 <prog.eslamadel@gmail.com>
2021-02-24 00:18:09 +0200

    add output file and upload script.

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="eslam2017/project-microservices:latest"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+docker tag project-microservices ${dockerpath}
+docker login
 # Step 3:
 # Push image to a docker repository
+docker push ${dockerpath}
