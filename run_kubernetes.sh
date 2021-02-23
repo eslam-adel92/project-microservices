07fd6f2384522c661b25ddec6d51a1a0999a27ed
eslam-adel92 <prog.eslamadel@gmail.com>
2021-02-24 00:50:48 +0200

    after adding k8s task.

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,21 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath="eslam2017/project-microservices"
 # Step 2
 # Run the Docker Hub container with kubernetes
 
+kubectl run project-microservices\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=project-microservices
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward project-microservices 8000:80
 
+
+# kubectl logs my-pod
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,7 +6,7 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-dockerpath="eslam2017/project-microservices:latest"
+dockerpath="eslam2017/project-microservices"
 
 # Step 2:  
 # Authenticate & tag
